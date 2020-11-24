a79a02f6934603ce5b79d72c5b516c9a703a6b2b
BTrevelyan <becky.trevelyan@gmail.com>
2020-11-24 13:24:46 -0800

    checkpoint commit

---
+++ src/api.js
@@ -0,0 +1,70 @@
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/ei-student';
+
+/**
+ * listApiFetch - Wrapper function for native `fetch` to standardize error handling. 
+ * @param {string} url 
+ * @param {object} options 
+ * @returns {Promise} - resolve on all 2xx responses with JSON body
+ *                    - reject on non-2xx and non-JSON response with 
+ *                      Object { code: Number, message: String }
+ */
+const listApiFetch = function (...args) {
+  let error;
+  return fetch(...args)
+    .then(res => {
+      if (!res.ok) {
+        error = { code: res.status };
+        if (!res.headers.get('content-type').includes('json')) {
+          error.message = res.statusText;
+          return Promise.reject(error);
+        }
+      }
+      return res.json();
+    })
+    .then(data => {
+      if (error) {
+        error.message = data.message;
+        return Promise.reject(error);
+      }
+      return data;
+    });
+};
+
+const getItems = function () {
+  return listApiFetch(`${BASE_URL}/items`);
+};
+
+const createItem = function (name) {
+  const newItem = JSON.stringify({ name });
+  return listApiFetch(`${BASE_URL}/items`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: newItem
+  });
+};
+
+const updateItem = function (id, updateData) {
+  const newData = JSON.stringify(updateData);
+  return listApiFetch(`${BASE_URL}/items/${id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: newData
+  });
+};
+
+const deleteItem = function (id) {
+  return listApiFetch(BASE_URL + '/items/' + id, {
+    method: 'DELETE'
+  });
+};
+
+export default {
+  getItems,
+  createItem,
+  updateItem,
+  deleteItem
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
 import $ from 'jquery';
 
 import 'normalize.css';
 import './index.css';
+import api from './api';
+import store from './store';
 
 import shoppingList from './shopping-list';
 
 const main = function () {
+  api.getItems()
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    });
+
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
